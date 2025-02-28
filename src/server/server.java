9acc1e23728a38d8fd70fd7ff5accc2abc514f81
mh <mh@localhost>
2025-02-28 13:11:09 +0900

    client_20250228-0110

--- src/server/server.java
+++ src/server/server.java
@@ -1,9 +1,38 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class server {
 
+	private void connectClient() {
+		
+	}
+	
+	public void fileReceive() {
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BufferedReader in = null;
+		PrintWriter out = null;
+		
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+		
+		try {
+			serverSocket = new ServerSocket(8000);
+			socket = serverSocket.accept();
+			
+			
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
