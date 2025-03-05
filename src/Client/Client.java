e2b58854335e89a9cdb5fb5b80d13fc68767a197
mh <mh@localhost>
2025-03-05 23:55:56 +0900

    20250305-2355: Bug fix

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -2,170 +2,127 @@ package Client;
 
 import java.io.*;
 import java.net.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Client {
-	private static final int serverPort = 8000;
-	private String serverAddr;
-	
-	public Client(String serverAddr) {
-		this.serverAddr = serverAddr;
-	}
-	
-	public int put(String key, String value, Long TTL) throws Exception {
-		DataOutputStream dos = null;
-		DataInputStream dis = null;
-		Socket socket = null;
-		
-		try {
-			socket = new Socket(serverAddr, serverPort);
-			dos = new DataOutputStream(socket.getOutputStream());
-			dis = new DataInputStream(socket.getInputStream());
-			
-			dos.writeInt(1);
-			dos.writeUTF(key);
-			dos.writeUTF(value);
-			dos.writeLong(TTL);
-						
-			return dis.readInt();
-		} catch (Exception e) {
-			throw new Exception(e);
-		} finally {
-			try {
-				if (socket != null) socket.close();
-				if (dos != null) dos.close();
-				if (dis != null) dis.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	public String get(String key) throws Exception {
-		DataOutputStream dos = null;
-		DataInputStream dis = null;
-		Socket socket = null;
-		
-		try {
-			socket = new Socket(serverAddr, serverPort);			
-			dos = new DataOutputStream(socket.getOutputStream());
-			dis = new DataInputStream(socket.getInputStream());
-						
-			dos.writeInt(2);
-			dos.writeUTF(key);
-			
-			int res = dis.readInt();
-			if (res == 1) {
-				return dis.readUTF();
-			} else {
-				return null;
-			}			
-		} catch (Exception e) {
-			throw new Exception(e);
-		} finally {
-			try {
-				if (socket != null) socket.close();
-				if (dos != null) dos.close();
-				if (dis != null) dis.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	public int remove(String key) throws Exception {
-		DataOutputStream dos = null;
-		DataInputStream dis = null;
-		Socket socket = null;
-		
-		try {
-			socket = new Socket(serverAddr, serverPort);
-			dos = new DataOutputStream(socket.getOutputStream());
-			dis = new DataInputStream(socket.getInputStream());
-						
-			dos.writeInt(3);
-			dos.writeUTF(key);
-			
-			return dis.readInt();
-		} catch (Exception e) {
-			throw new Exception(e);
-		} finally {
-			try {
-				if (socket != null) socket.close();
-				if (dos != null) dos.close();
-				if (dis != null) dis.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		Client client = new Client("localhost");
-		Scanner scanner = new Scanner(System.in);
+    private final String serverAddress;
+    private final int port;
+
+    public Client(String address, int port) {
+        this.serverAddress = address;
+        this.port = port;
+    }
+
+    public boolean put(String key, String value, int ttl) throws Exception {
+        try (Socket socket = new Socket(serverAddress, port);
+             DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+             DataInputStream dis = new DataInputStream(socket.getInputStream())) {
+
+        	dos.writeUTF("PUT");
+        	dos.writeUTF(key);
+        	dos.writeUTF(value);
+            dos.writeInt(ttl);
+
+            return dis.readInt() == 0;
+        }
+    }
+
+    public String get(String key) throws Exception {
+        try (Socket socket = new Socket(serverAddress, port);
+             DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+             DataInputStream dis = new DataInputStream(socket.getInputStream())) {
+
+        	dos.writeUTF("GET");  
+        	dos.writeUTF(key);
+
+            if (dis.readInt() == 0) {
+                return dis.readUTF();
+            }
+            return null;
+        }
+    }
+
+    public boolean remove(String key) throws Exception {
+        try (Socket socket = new Socket(serverAddress, port);
+             DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+             DataInputStream dis = new DataInputStream(socket.getInputStream())) {
+
+        	dos.writeUTF("REMOVE");
+        	dos.writeUTF(key);
+        	
+            return dis.readInt() == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        String serverAddress = "localhost";
+        int serverPort = 8000;
+
+        Client client = new Client(serverAddress, serverPort);
+        Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Input (put / get / remove / exit)");
-            String command = scanner.nextLine().trim();
-
+            System.out.println("1. PUT");
+            System.out.println("2. GET");
+            System.out.println("3. REMOVE");
+            System.out.println("4. FINISH");
+            System.out.print("SELECT: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println();
+            
+            String key, value;
+            int ttl;
+            
             try {
-                if (command.equals("put")) {
-                    System.out.print("Key: ");
-                    String key = scanner.nextLine().trim();
-                    
-                    System.out.print("Value: ");
-                    String value = scanner.nextLine().trim();
-                    
-                    System.out.print("TTL: ");
-                    long ttl = Long.parseLong(scanner.nextLine().trim());
-                    
-                    try {
-                    	int res = client.put(key, value, ttl);
-                    	if (res == 1) {
-                    		System.out.println("SUCCESS");
-                    	} else {
-                    		System.out.println("FAIL");
-                    	}
-                    } catch (Exception e) {
-                    	e.printStackTrace();
-                    }
-                } else if (command.equals("get")) {
-                    System.out.print("Key: ");
-                    String key = scanner.nextLine().trim();
-                    
-                    try {
-                    	String res = client.get(key);
-                    	if (res != null) {
-                    		System.out.println("Value: " + res);
-                    	} else {
-                    		System.out.println("Not found");
-                    	}
-                    } catch (Exception e) {
-                    	e.printStackTrace();
-                    }
-                } else if (command.equals("remove")) {
-                    System.out.print("Key: ");
-                    String key = scanner.nextLine().trim();
-                    
-                    try {
-                    	int res = client.remove(key);
-                    	if (res == 1) {
-                    		System.out.println("SUCCESS");
-                    	} else {
-                    		System.out.println("FAIL");
-                    	}
-                    } catch (Exception e) {
-                    	e.printStackTrace();
-                    }                    
-                } else if (command.equals("exit")) {
-                    break;
-                } else {
-                    System.out.println("Wrong command!");
+                switch (choice) {
+                    case 1:
+                        System.out.print("KEY: ");
+                        key = scanner.nextLine();
+                        System.out.print("VALUE: ");
+                        value = scanner.nextLine();
+                        System.out.print("TTL: ");
+                        ttl = scanner.nextInt();
+                        scanner.nextLine();
+                        if (client.put(key, value, ttl)) {
+                            System.out.println("SUCCESS");
+                        } else {
+                            System.out.println("FAIL");
+                        }
+                        break;
+
+                    case 2:
+                        System.out.print("KEY: ");
+                        key = scanner.nextLine();
+                        value = client.get(key);
+                        if (value != null) {
+                            System.out.println("VALUE: " + value);
+                        } else {
+                            System.out.println("NOT FOUND");
+                        }
+                        break;
+
+                    case 3:
+                        System.out.print("KEY: ");
+                        key = scanner.nextLine();
+                        if (client.remove(key)) {
+                            System.out.println("SUCCESS");
+                        } else {
+                            System.out.println("FAIL");
+                        }
+                        break;
+
+                    case 4:
+                        System.out.println("FINISHED...");
+                        scanner.close();
+                        return;
+
+                    default:
+                        System.out.println("PLEASE INPUT THE CORRECT COMMAND");
                 }
             } catch (Exception e) {
-            	e.printStackTrace();
+                e.printStackTrace();
             }
         }
-        scanner.close();
-	}
+    }
 }
--- src/Server/Server.java
+++ src/Server/Server.java
@@ -2,188 +2,168 @@ package Server;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
 import java.util.concurrent.*;
 
 public class Server {
-	private static final int serverPort = 8000;
-	private static final String logFile = "data.log";
-	private static ConcurrentHashMap<String, String> hashMap = new ConcurrentHashMap();
-	private static ConcurrentHashMap<String, Long> TTLs = new ConcurrentHashMap();
-	private static final long TTLInterval = 1000;
-	
-	private static synchronized void saveLogs() {	// hash map to log file
-		File file = new File(logFile);
-		
-		FileOutputStream fos = null;
-		BufferedWriter bw = null;
-		
-		try {			
-			fos = new FileOutputStream(file);
-			bw = new BufferedWriter(new OutputStreamWriter(fos));
-				
-			for (var e : hashMap.entrySet()) {
-				bw.write(e.getKey() + ":" + e.getValue());
+    private static final int PORT = 8000;
+    private static final String DATA_FILE = "data.txt";
+
+    private final ConcurrentHashMap<String, VALUE> hashMap = new ConcurrentHashMap<String, VALUE>();
+    private final ScheduledExecutorService cleaner = Executors.newScheduledThreadPool(1);
+
+    public static void main(String[] args) {
+        new Server().start();
+    }
+
+    public void start() {
+    	loadLogs();
+        
+        cleaner.scheduleAtFixedRate(this::removeExpiredEntries, 1, 1, TimeUnit.SECONDS);
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server started...");
+            
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                new Thread(new ClientHandler(clientSocket)).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        saveLogs();
+    }
+
+    private class ClientHandler implements Runnable {
+        private final Socket socket;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try (DataInputStream dis = new DataInputStream(socket.getInputStream());
+                 DataOutputStream dos = new DataOutputStream(socket.getOutputStream())) {
+            	
+                String operation = dis.readUTF();
+                String key, value;
+                int ttl;
+                
+                if (operation.equals("PUT")) {
+                	key = dis.readUTF();
+                    value = dis.readUTF();
+                    ttl = dis.readInt();
+                    
+                    dos.writeInt(put(key, value, ttl) ? 0 : -1);
+                } else if (operation.equals("GET")) {
+                	key = dis.readUTF();
+                    value = get(key);
+                    if (value != null) {
+                    	dos.writeInt(0);
+                    	dos.writeUTF(value);
+                    } else {
+                    	dos.writeInt(1);
+                    }
+                } else if (operation.equals("REMOVE")) {
+                    key = dis.readUTF();
+                    boolean removed = remove(key);
+                    dos.writeInt(removed ? 0 : 1);
+                } else {
+                	dos.writeInt(-1);                	
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean put(String key, String value, int ttl) {
+    	if (hashMap.get(key) == null) return false;
+    	if (ttl <= 0) return true;
+    	
+        long expire = System.currentTimeMillis() + (ttl * 1000);
+        hashMap.put(key, new VALUE(value, expire));
+        saveLogs();
+        
+        return true;
+    }
+
+    private String get(String key) {
+    	VALUE entry = hashMap.get(key);
+    	
+    	if (entry == null) return null;
+    	
+    	if (System.currentTimeMillis() > entry.ttl) {
+        	hashMap.remove(key);
+        	saveLogs();
+            return null;
+        }
+        
+        return entry.value;
+    }
+
+    private boolean remove(String key) {
+    	if (hashMap.remove(key) == null) return false;
+    	saveLogs();
+        return true;
+    }
+
+    private void removeExpiredEntries() {
+        long current = System.currentTimeMillis();
+        for (String key : hashMap.keySet()) {
+        	VALUE entry = hashMap.get(key);
+            if (entry != null && current > entry.ttl) {
+            	hashMap.remove(key);
+            }
+        }
+    }
+
+    private void saveLogs() {
+    	File file = new File(DATA_FILE);
+    	
+        try (FileOutputStream fos = new FileOutputStream(file);
+        	 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos))) {
+            for (var e : hashMap.entrySet()) {
+				bw.write(e.getKey() + ":" + e.getValue().value + ":" + e.getValue().ttl);
 				bw.newLine();
 			}
 			bw.flush();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (fos != null) fos.close();
-				if (bw != null) bw.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	private static synchronized void loadLogs() {	// log file to hash map
-		File file = new File(logFile);
-		if (!file.exists()) return;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-		FileInputStream fis = null;
-		BufferedReader br = null;
-		
-		try {
-			fis = new FileInputStream(file);
-			br = new BufferedReader(new InputStreamReader(fis));
-				
-			String line;
-			while ((line = br.readLine()) != null) {
-				String[] parts = line.split(":", 2);
-				if (parts.length == 2) {
-					hashMap.put(parts[0], parts[1]);
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (fis != null) fis.close();
-				if (br != null) br.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-	private static void TTLCheck() {	// TTL check
-		Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
-            long currentTime = System.currentTimeMillis();
+    private void loadLogs() {
+        File file = new File(DATA_FILE);
+        if (!file.exists()) return;
+
+        try (FileInputStream fis = new FileInputStream(file);
+        	 BufferedReader br = new BufferedReader(new InputStreamReader(fis))) {
+            String line;
             
-            for (var entry : TTLs.entrySet()) {
-                if (entry.getValue() <= currentTime) {
-                    hashMap.remove(entry.getKey());
-                    TTLs.remove(entry.getKey());
-                    saveLogs();
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(":");
+                if (parts.length == 3) {
+                    String key = parts[0];
+                    String value = parts[1];
+                    long ttl = Long.parseLong(parts[2]);
+                    hashMap.put(key, new VALUE(value, ttl));
                 }
             }
-        }, TTLInterval, TTLInterval, TimeUnit.MILLISECONDS);
-	}
-	
-	private static class Handler extends Thread {
-		private Socket socket;
-		
-		public Handler(Socket socket) {
-			this.socket = socket;
-		}
-		
-		@Override
-		public void run() {    		
-			DataInputStream dis = null;
-			DataOutputStream dos = null;
-			
-			try {
-				dis = new DataInputStream(socket.getInputStream());
-				dos = new DataOutputStream(socket.getOutputStream());
-				
-				int operation = dis.readInt();
-				String key, value;
-				long TTL;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-				if (operation == 1) {	// put
-					key = dis.readUTF();
-					value = dis.readUTF();
-					TTL = dis.readLong();
-					
-					hashMap.put(key, value);
-					TTLs.put(key, TTL);					
-					saveLogs();
-					
-					dos.writeInt(1);
-				} else if (operation == 2) {	// get
-					key = dis.readUTF();
-					value = hashMap.get(key);
-					
-					if (value != null) {
-						dos.writeInt(1);
-						dos.writeUTF(value);
-					} else {
-						dos.writeInt(0);
-					}
-				} else if (operation == 3) {	// remove
-					key = dis.readUTF();
-					
-					if (hashMap.remove(key) != null) {
-						TTLs.remove(key);
-						saveLogs();
-						dos.writeInt(1);
-					} else {
-						dos.writeInt(0);
-					}
-				} else {
-					dos.writeInt(0);
-				}
-				
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					if (dis != null) dis.close();
-					if (dos != null) dos.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}		
-	}
-	
-	public static void main(String[] args) {
-		loadLogs();
-		TTLCheck();
-		
-		ServerSocket serverSocket = null;
-		
-		try {
-			serverSocket = new ServerSocket(serverPort);
-			
-            System.out.println("Server started...");
-			
-            while (true) {
-            	Socket socket = null;
-            	
-            	try {
-            		socket = serverSocket.accept();
-                    new Handler(socket).start();
-            	} catch (Exception e) {
-            		e.printStackTrace();
-            	} finally {
-            		try {
-            			if (socket != null) socket.close();
-            		} catch (Exception e) {
-            			e.printStackTrace();
-            		}
-            	}
-            }			
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (serverSocket != null) serverSocket.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    private static class VALUE {
+        String value;
+        long ttl;
+
+        VALUE(String value, long ttl) {
+            this.value = value;
+            this.ttl = ttl;
+        }
+    }
 }
